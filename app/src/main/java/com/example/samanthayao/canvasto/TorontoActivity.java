b7d899c1c01e1ae3eb1cdc1568756e13220c84ba
Samantha Yao <yaosa@utschools.ca>
2017-05-10 14:33:18 -0400

    Made a grid

--- app/src/main/java/com/example/samanthayao/canvasto/TorontoActivity.java
+++ app/src/main/java/com/example/samanthayao/canvasto/TorontoActivity.java
@@ -1,8 +1,18 @@
 package com.example.samanthayao.canvasto;
 
+import android.os.Bundle;
+import android.os.Build;
+import android.support.v7.app.AppCompatActivity;
+
 /**
  * Created by Samantha Yao on 5/10/2017.
  */
 
-public class TorontoActivity {
+public class TorontoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_toronto);
+    }
 }
