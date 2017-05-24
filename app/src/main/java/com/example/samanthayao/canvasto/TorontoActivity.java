543fbfeecc640bdd19f40c0c339b2d66723de69b
Samantha Yao <yaosa@utschools.ca>
2017-05-24 12:10:12 -0400

    location of stuff is messed up

--- app/src/main/java/com/example/samanthayao/canvasto/TorontoActivity.java
+++ app/src/main/java/com/example/samanthayao/canvasto/TorontoActivity.java
@@ -30,9 +30,9 @@ public class TorontoActivity extends AppCompatActivity {
     TimerTask updateMoneyTimer;
     GridView gv;
     String[] plots = new String[]{
-            "A1","A2","A3","A4","A5","B1","B2","B3","B4","B5",
-            "C1","C2","C3","C4","C5","D1","D2","D3","D4","D5",
-            "E1","E2","E3","E4","E5"
+            "A1","A2","A3","A4","A5","B1","B2","B3","B4"//,"B5",
+            //"C1","C2","C3","C4","C5","D1","D2","D3","D4","D5",
+            //"E1","E2","E3","E4","E5"
     };
     @Override
     protected void onCreate(Bundle savedInstanceState){
