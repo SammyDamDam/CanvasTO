821203cab7a9aca1e35feb49630b41c394e7248c
Samantha Yao <yaosa@utschools.ca>
2017-05-25 13:45:16 -0400

    Added build class, timer is buggy (app crashes)

---
+++ app/src/main/java/com/example/samanthayao/canvasto/BuildActivity.java
@@ -0,0 +1,18 @@
+package com.example.samanthayao.canvasto;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Samantha Yao on 5/25/2017.
+ */
+
+public class BuildActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_build);
+
+    }
+}
--- app/src/main/java/com/example/samanthayao/canvasto/TorontoActivity.java
+++ app/src/main/java/com/example/samanthayao/canvasto/TorontoActivity.java
@@ -33,9 +33,9 @@ public class TorontoActivity extends AppCompatActivity {
     TextView mtreasuryValue;
     GridView gv;
     String[] plots = new String[]{
-            "A1", "A2", "A3", "A4", "A5", "B1", "B2", "B3", "B4"//,"B5",
-            //"C1","C2","C3","C4","C5","D1","D2","D3","D4","D5",
-            //"E1","E2","E3","E4","E5"
+            "A1", "A2", "A3", "A4", "A5", "B1", "B2", "B3", "B4","B5",
+            "C1","C2","C3","C4","C5","D1","D2","D3","D4","D5",
+            "E1","E2","E3","E4","E5"
     };
 
     @Override
@@ -76,7 +76,7 @@ public class TorontoActivity extends AppCompatActivity {
 
         mtreasuryValue = (TextView) findViewById(R.id.treasuryValue);
         treasury= 0;
-        updateTreasuryTimer = new TimerTask(){
+        /*updateTreasuryTimer = new TimerTask(){
             @Override
             public void run(){
                 treasury += 1000;
@@ -84,7 +84,7 @@ public class TorontoActivity extends AppCompatActivity {
                 mtreasuryValue.setText(treasury);
             }
         };
-        treasuryTimer.schedule(updateTreasuryTimer,0,10000);
+        treasuryTimer.schedule(updateTreasuryTimer,0,10000);*/
 
     }
 }
