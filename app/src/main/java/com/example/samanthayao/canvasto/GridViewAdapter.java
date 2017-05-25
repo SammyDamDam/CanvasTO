f56a5c1d8ebc8bdcc4e47d1db3db30afcdd258a5
Samantha Yao <yaosa@utschools.ca>
2017-05-25 14:38:14 -0400

    Adding images to GridView in progress

---
+++ app/src/main/java/com/example/samanthayao/canvasto/GridViewAdapter.java
@@ -0,0 +1,33 @@
+package com.example.samanthayao.canvasto;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Samantha Yao on 5/25/2017.
+ */
+
+public class GridViewAdapter extends ArrayAdapter {
+    private Context context;
+    private int layoutResourceId;
+    private ArrayList data = new ArrayList();
+
+    public GridViewAdapter(Context context, int layoutResourceId, ArrayList data){
+        super(context, layoutResourceId, data);
+        this.layoutResourceId = layoutResourceId;
+        this.context = context;
+        this.data = data;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent){
+        View row = convertView;
+        RecyclerView.ViewHolder holder = null;
+    }
+
+}
