10aed6365d0ff501dcd3f1521694961b838512a5
Samantha Yao <yaosa@utschools.ca>
2017-06-03 14:19:09 -0400

    JSONParser still not working

--- app/src/main/java/com/example/samanthayao/canvasto/CalendarActivity.java
+++ app/src/main/java/com/example/samanthayao/canvasto/CalendarActivity.java
@@ -11,16 +11,20 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.android.volley.Request;
+import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.VolleyLog;
 import com.android.volley.toolbox.JsonArrayRequest;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+
+import java.util.ArrayList;
 
 /**
  * Created by Samantha Yao on 6/2/2017.
@@ -28,111 +32,63 @@ import org.json.JSONObject;
 
 public class CalendarActivity extends AppCompatActivity{
 
-    // json object response url
-    private String URL = "http://app.toronto.ca/cc_sr_v1_app/data/edc_eventcal_APR?limit=500";
-
-    private static String TAG = CalendarActivity.class.getSimpleName();
-    private Button mjsonRequestBtn;
-
-    // Progress dialog
-    private ProgressDialog pDialog;
-
-    private TextView mjsonText;
-
-    // temporary string to show the parsed response
-    private String jsonResponse;
+    // Will show the string "data" that holds the results
+    TextView results;
+    // URL of object to be parsed
+    String JsonURL = "http://app.toronto.ca/cc_sr_v1_app/data/edc_eventcal_APR?limit=500.json";
+    // This string will hold the results
+    String data = "";
+    // Defining the Volley request queue that handles the URL request concurrently
+    RequestQueue requestQueue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calendar);
+        // Creates the Volley request queue
+        requestQueue = Volley.newRequestQueue(this);
 
-        mjsonRequestBtn = (Button) findViewById(R.id.jsonRequestBtn);
-        mjsonText = (TextView) findViewById(R.id.jsonText);
+        // Casts results into the TextView found within the main layout XML with id jsonData
+        results = (TextView) findViewById(R.id.jsonData);
 
-        pDialog = new ProgressDialog(this);
-        pDialog.setMessage("Please wait...");
-        pDialog.setCancelable(false);
-
-        mjsonRequestBtn.setOnClickListener(new View.OnClickListener() {
-
-            @Override
-            public void onClick(View v) {
-                // making json object request
-                makeJsonArrayRequest();
-            }
-        });
-
-    }
-
-    /**
-     * Method to make json object request where json response starts wtih {
-     * */
-    private void makeJsonArrayRequest() {
-
-        showpDialog();
-
-        JsonArrayRequest req = new JsonArrayRequest(URL,
+        // Creating the JsonArrayRequest class called arrayreq, passing the required parameters
+        //JsonURL is the URL to be fetched from
+        JsonArrayRequest arrayreq = new JsonArrayRequest(JsonURL,
+                // The second parameter Listener overrides the method onResponse() and passes
+                //JSONArray as a parameter
                 new Response.Listener<JSONArray>() {
+
+                    // Takes the response from the JSON request
                     @Override
                     public void onResponse(JSONArray response) {
-                        Log.d(TAG, response.toString());
-
                         try {
-                            // Parsing json array response
-                            // loop through each json object
-                            jsonResponse = "";
-                            for (int i = 0; i < response.length(); i++) {
+                            //TODO retrieve stuff from the JSON file
+                            // Retrieves first JSON object in outer array
+                            JSONObject calEvent = response.getJSONObject(0);
 
-                                JSONObject event = (JSONObject) response.get(i);
+                            JSONObject orgType = calEvent.getJSONObject("orgType");
 
-                                JSONObject calEvent = event.getJSONObject("calEvent");
-                                String orgType = calEvent.getString("orgType");
-                                String startDate = calEvent.getString("startDate");
-                                String orgAddress = calEvent.getString("orgAddress");
-
-                                //JSONObject phone = event.getJSONObject("phone");
-                                //String home = phone.getString("home");
-                                //String mobile = phone.getString("mobile");
-
-                                jsonResponse += "OrgType: " + orgType + "\n\n";
-                                jsonResponse += "Start: " + startDate + "\n\n";
-                                jsonResponse += "Address: " + orgAddress + "\n\n";
-
-                            }
-
-                            mjsonText.setText(jsonResponse);
-
-                        } catch (JSONException e) {
-                            e.printStackTrace();
-                            Toast.makeText(getApplicationContext(),
-                                    "Error: " + e.getMessage(),
-                                    Toast.LENGTH_LONG).show();
+                            // Adds the data string to the TextView "results"
+                            results.setText(calEvent.toString());
                         }
-
-                        hidepDialog();
+                        // Try and catch are included to handle any errors due to JSON
+                        catch (JSONException e) {
+                            // If an error occurs, this prints the error to the log
+                            e.printStackTrace();
+                        }
                     }
-                }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-                VolleyLog.d(TAG, "Error: " + error.getMessage());
-                Toast.makeText(getApplicationContext(),
-                        error.getMessage(), Toast.LENGTH_SHORT).show();
-                hidepDialog();
-            }
-        });
-
-        // Adding request to request queue
-        AppController.getInstance().addToRequestQueue(req);
-    }
-
-    private void showpDialog() {
-        if (!pDialog.isShowing())
-            pDialog.show();
-    }
-
-    private void hidepDialog() {
-        if (pDialog.isShowing())
-            pDialog.dismiss();
+                },
+                // The final parameter overrides the method onErrorResponse() and passes VolleyError
+                //as a parameter
+                new Response.ErrorListener() {
+                    @Override
+                    // Handles errors that occur due to Volley
+                    public void onErrorResponse(VolleyError error) {
+                        Log.e("Volley", "Error");
+                    }
+                }
+        );
+        // Adds the JSON array request "arrayreq" to the request queue
+        requestQueue.add(arrayreq);
     }
 }
